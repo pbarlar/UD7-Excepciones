991cb6c6073ae7372c1f097f0b8bf2d27c3adab5
pbarlar <pbarlar@g.educaand.es>
2025-04-22 11:02:35 +0200

    Update ej3.java

--- Ejercicio3/ej3.java
+++ Ejercicio3/ej3.java
@@ -1,7 +1,22 @@
 package Ejercicio3;
 
+import java.util.Scanner;
+
 public class ej3 {
     public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        String cadena=entrada.nextLine();
+        int num=entrada.nextInt();
+        boolean repetir=false;
         
+        do {
+            try {
+                
+            } catch (Exception e) {
+                
+            }
+        } while (repetir);
+
+
     }
 }
